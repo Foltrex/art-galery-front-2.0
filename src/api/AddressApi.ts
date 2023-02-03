988f60f20a27768f61765f880817042485b8d476
Foltrex <fpm.chernenkAV@bsu.by>
2023-02-03 11:05:49 +0300

    Delete AddressApi class

--- src/api/AddressApi.ts
+++ src/api/AddressApi.ts
@@ -5,10 +5,3 @@ import { ART_SERVICE, axiosApi } from "../http/axios";
 export const useGetAllAddressesByCityId = (id: string) => {
     return useFetch<Address[]>(`${ART_SERVICE}/addresses/city/${id}`);
 }
-
-export class AddressApi {
-
-    static getAllAddressesByCityId(id: string) {
-        return axiosApi.get<Address[]>(`${ART_SERVICE}/addresses/city/${id}`)
-    }
-}
