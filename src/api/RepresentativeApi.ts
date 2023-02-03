33616f3d22e045864c3c60ffb0cee04b7778edb3
Foltrex <fpm.chernenkAV@bsu.by>
2023-02-03 11:03:13 +0300

    Delete representative api

--- src/api/RepresentativeApi.ts
+++ src/api/RepresentativeApi.ts
@@ -2,10 +2,6 @@ import { Representative } from "../entities/representative";
 import { IPage, useDelete, useFetch, usePost } from "../hooks/react-query";
 import { ART_SERVICE, axiosApi } from "../http/axios";
 
-interface TPageableResponse {
-    content: Representative[];
-}
-
 export const useGetRepresentativesPageByAccountId = (accountId: string, page?: number, size?: number) => {
     return useFetch<IPage<Representative>>(
         `${ART_SERVICE}/representatives/account/${accountId}`,
@@ -21,31 +17,4 @@ export const useDeleteRepresentative = () => {
 
 export const useSaveRepresentative = () => {
     return usePost<Representative>(`${ART_SERVICE}/representatives`);
-}
-
-export class RepresentativeApi {
-
-    static getAllRepresentative(page?: number, size?: number) {
-        return axiosApi.get<any>(`${ART_SERVICE}/representatives`, {
-            params: {
-                page: page,
-                size: size,
-            }
-        })
-    }
-
-    static getAllRepresentativeByOrganizationId(organizationId: string, page?: number, size?: number) {
-        return axiosApi.get<any>(`${ART_SERVICE}/representatives/organizations/${organizationId}`, {
-            params: {
-                page: page,
-                size: size,
-            }
-        }).then(response => response.data)
-    }
-
-
-    static deleteById(id: string) {
-        return axiosApi.delete<any>(`${ART_SERVICE}/representatives/${id}`);
-    }
-
 }
