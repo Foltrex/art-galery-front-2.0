9298f0e77c5e3f4620c65eb0dee51aa2a08eb710
maxim-semenko <maks.semenko@gmail.com>
2023-02-15 11:18:09 +0300

    add contentText props to DeleteModal.tsx

--- src/components/modal/DeleteModal.tsx
+++ src/components/modal/DeleteModal.tsx
@@ -1,35 +1,38 @@
-import { Dialog, DialogTitle, Divider, DialogContent, DialogContentText, DialogActions, Button } from '@mui/material';
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider} from '@mui/material';
 import * as React from 'react';
 
 interface IDeleteModalProps {
-	open: boolean;
-	onClose: () => void;
-	onDelete: () => void;
+    open: boolean;
+    onClose: () => void;
+    onDelete: () => void;
+    contextText?: string;
 }
 
-const DeleteModal: React.FunctionComponent<IDeleteModalProps> = ({ open, onClose, onDelete }) => {
-	const handleDelete = () => {
-		onDelete();
-		onClose();
-	}
+const DeleteModal: React.FunctionComponent<IDeleteModalProps> = ({open, onClose, onDelete, contextText}) => {
+    const handleDelete = () => {
+        onDelete();
+        onClose();
+    }
 
-	return (
-		<Dialog open={open}>
-			<DialogTitle>
-				Are you sure?
-			</DialogTitle>
-			<Divider />
-			<DialogContent>
-				<DialogContentText>
-					Do you really want to delete this record? This action cannot be undone.
-				</DialogContentText>
-			</DialogContent>
-			<DialogActions>
-				<Button onClick={onClose}>Cancel</Button>
-				<Button onClick={handleDelete} variant='contained'>Delete</Button>
-			</DialogActions>
-		</Dialog>
-	);
+    return (
+        <Dialog open={open}>
+            <DialogTitle>
+                Are you sure?
+            </DialogTitle>
+            <Divider/>
+            <DialogContent>
+                <DialogContentText>
+                    {contextText !== undefined ?
+                        contextText : 'Do you really want to delete this record? This action cannot be undone.'
+                    }
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button onClick={handleDelete} variant='contained'>Delete</Button>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 export default DeleteModal;
