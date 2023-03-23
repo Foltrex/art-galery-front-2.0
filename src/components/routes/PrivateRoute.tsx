b23e2f10aa2d2020bb0733410f301636b2a7cffa
rs <rs@rs.com>
2023-03-23 23:10:29 +0400

    deploy with context path "admin"

--- src/components/routes/PrivateRoute.tsx
+++ src/components/routes/PrivateRoute.tsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute: React.FC<PropsWithChildren> = ({children}) => {
     const [cookies] = useCookies(['token']);
 
-    return <>{cookies.token ? children : <Navigate to='/admin/auth/signin' />}</>;
+    return <>{cookies.token ? children : <Navigate to='/auth/signin' />}</>;
 }
 
 export default PrivateRoute;
