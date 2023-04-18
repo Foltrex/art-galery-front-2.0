690e9bd9164600cc7ec9cedd65650105e1c75394
rs <rs@rs.com>
2023-04-18 21:59:06 +0400

    reset password and delete account

--- src/pages/users/components/ProfileInfo.tsx
+++ src/pages/users/components/ProfileInfo.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
 import {Account} from "../../../entities/account";
 import {Button, TextField} from "@mui/material";
 import MetadataList, {prepareAccountProperties} from "./MetadataList";
@@ -11,10 +11,20 @@ import {Form, Formik} from "formik";
 import * as yup from "yup";
 import Bubble from "../../../components/bubble/Bubble";
 import {UserTypeDropdown} from "../../../components/form/UserTypeDropdown";
+import ChangePasswordDialog from "./ChangePasswordDialog";
+import DeleteModal from "../../../components/modal/DeleteModal";
+import {useDeleteAccountById} from "../../../api/AccountApi";
+import {useNavigate} from "react-router-dom";
+import {AuthService} from "../../../services/AuthService";
+import {getErrorMessage} from "../../../util/PrepareDataUtil";
 
 
-const ProfileInfo = (props: { account: Account, onSubmit:(a:Account) => Promise<boolean>, back?:() => void }) => {
+const ProfileInfo = (props: { account: Account, onSubmit: (a: Account) => Promise<boolean>, back?: () => void }) => {
     const {authStore} = useRootStore();
+    const [showResetPassword, setShowResetPassword] = useState(false);
+    const [showDeleteAccount, setShowDeleteAccount] = useState(false);
+    const navigate = useNavigate();
+
     const isBoss = useMemo(() => {
         if (authStore.account.accountType !== AccountEnum.REPRESENTATIVE || props.account.accountType !== AccountEnum.REPRESENTATIVE) {
             return false;
@@ -50,6 +60,26 @@ const ProfileInfo = (props: { account: Account, onSubmit:(a:Account) => Promise<
         )
     })
 
+    const mutationDelete = useDeleteAccountById();
+    const onDelete = async () => {
+        try {
+            await mutationDelete.mutateAsync(props.account!.id);
+            if (props.account.id === authStore.account.id) {
+                AuthService.logout()
+                navigate("/auth/signin")
+            } else {
+                if (props.back) {
+                    props.back();
+                } else {
+                    navigate("/")
+                }
+            }
+        } catch (e) {
+            // add push notification
+            Bubble.error("Failed to delete account. Error message is " + getErrorMessage(e));
+        }
+    }
+
     const canEditEmail = !props.account.id || authStore.account.accountType === AccountEnum.SYSTEM;
 
     return (
@@ -69,7 +99,7 @@ const ProfileInfo = (props: { account: Account, onSubmit:(a:Account) => Promise<
                 values.metadata = values.metadata.filter(m => metadataMap[m.key]);
 
                 props.onSubmit(values).then((result) => {
-                    if(result) {
+                    if (result) {
                         Bubble.success("Account was updated");
                     }
                     setSubmitting(false)
@@ -80,66 +110,99 @@ const ProfileInfo = (props: { account: Account, onSubmit:(a:Account) => Promise<
                 <Form noValidate>
                     <table className={"profile-table"}>
                         <tbody>
-                            <tr>
-                                <td><strong>Email</strong></td>
-                                <td>{canEditEmail
-                                    ? <TextField fullWidth margin="dense" size={"small"} required
-                                                 name={"email"}
-                                                //  label={"Email"}
-                                                 value={formik.values.email}
-                                                 onChange={formik.handleChange}
-                                                 error={!!formik.errors.email} helperText={formik.errors.email}
-                                    />
-                                    : <div style={{paddingTop: 8, marginBottom: 5}}>{props.account?.email}</div>}</td>
-                            </tr>
-                            {authStore.account.accountType === AccountEnum.SYSTEM && <tr>
-                                <td><strong>User type</strong></td>
-                                <td><UserTypeDropdown value={formik.values.accountType} onChange={e => formik.setFieldValue("accountType", e, false)} /></td>
-                            </tr>}
-                            <tr>
-                                <td><strong>Name</strong></td>
-                                <td>{!canEdit && <span>{props.account?.firstName}&nbsp;{props.account?.lastName}</span>}
-                                    {canEdit && <div>
-                                        <TextField fullWidth margin="dense" size={"small"} required
-                                                   name={"firstName"}
-                                                   label={"Firstname"}
-                                                   value={formik.values.firstName}
-                                                   onChange={formik.handleChange}
-                                                   error={!!formik.errors.firstName} helperText={formik.errors.firstName}
-                                        />
-                                    </div>}</td>
-                            </tr>
-                            {canEdit && <tr>
-                                <td/><td>
-                                    <TextField fullWidth margin="dense"  size={"small"} required
-                                               name={"lastName"}
-                                               label={"Lastname"}
-                                               value={formik.values.lastName}
+                        <tr>
+                            <td><strong>Email</strong></td>
+                            <td>{canEditEmail
+                                ? <TextField fullWidth margin="dense" size={"small"} required
+                                             name={"email"}
+                                    //  label={"Email"}
+                                             value={formik.values.email}
+                                             onChange={formik.handleChange}
+                                             error={!!formik.errors.email} helperText={formik.errors.email}
+                                />
+                                : <div style={{paddingTop: 8, marginBottom: 5}}>{props.account?.email}</div>}</td>
+                        </tr>
+                        {authStore.account.accountType === AccountEnum.SYSTEM && <tr>
+                            <td><strong>User type</strong></td>
+                            <td><UserTypeDropdown value={formik.values.accountType}
+                                                  onChange={e => formik.setFieldValue("accountType", e, false)}/></td>
+                        </tr>}
+                        <tr>
+                            <td><strong>Name</strong></td>
+                            <td>{!canEdit && <span>{props.account?.firstName}&nbsp;{props.account?.lastName}</span>}
+                                {canEdit && <div>
+                                    <TextField fullWidth margin="dense" size={"small"} required
+                                               name={"firstName"}
+                                               label={"Firstname"}
+                                               value={formik.values.firstName}
                                                onChange={formik.handleChange}
+                                               error={!!formik.errors.firstName} helperText={formik.errors.firstName}
                                     />
-                                </td>
-                            </tr>}
-                            <MetadataList account={formik.values} metadata={formik.values.metadata} canEdit={canEdit} onChange={(key, value) => {
-                                const metadata = [...formik.values.metadata];
-                                let updated = false;
-                                for(let i = 0; i < metadata.length; i++) {
-                                    if(metadata[i].key === key) {
-                                        metadata[i] = {...metadata[i], value: value || ''}
-                                        updated = true;
-                                        break;
-                                    }
-                                }
-                                if(!updated) {
-                                    metadata.push({key: key, value: value || ''})
-                                }
-                                formik.setFieldValue('metadata', metadata, false);
-                            }}/>
-                            {canEdit && <tr>
-                                <td colSpan={2} style={{textAlign: "center"}}>
-                                    {props.back !== undefined && <Button type={"button"} onClick={() => props.back && props.back()}>Back</Button>}
-                                    <Button type={"submit"}>Save</Button>
-                                </td>
-                            </tr>}
+                                </div>}</td>
+                        </tr>
+                        {canEdit && <tr>
+                            <td/>
+                            <td>
+                                <TextField fullWidth margin="dense" size={"small"} required
+                                           name={"lastName"}
+                                           label={"Lastname"}
+                                           value={formik.values.lastName}
+                                           onChange={formik.handleChange}
+                                />
+                            </td>
+                        </tr>}
+                        <MetadataList account={formik.values} metadata={formik.values.metadata} canEdit={canEdit}
+                                      onChange={(key, value) => {
+                                          const metadata = [...formik.values.metadata];
+                                          let updated = false;
+                                          for (let i = 0; i < metadata.length; i++) {
+                                              if (metadata[i].key === key) {
+                                                  metadata[i] = {...metadata[i], value: value || ''}
+                                                  updated = true;
+                                                  break;
+                                              }
+                                          }
+                                          if (!updated) {
+                                              metadata.push({key: key, value: value || ''})
+                                          }
+                                          formik.setFieldValue('metadata', metadata, false);
+                                      }}/>
+                        {showResetPassword &&
+                            <ChangePasswordDialog open={true} onClose={() => setShowResetPassword(false)}/>}
+                        {showDeleteAccount && <DeleteModal open={true}
+                                                           contextText={(authStore.account.id === props.account.id
+                                                                   ? "You are about to delete your own account."
+                                                                   : "You are about to delete " + (props.account.firstName || "") + " " + (props.account.lastName || "") + " account.")
+                                                               + " All related information would be lost, and that action can not be undone. Are you sure to proceed?"
+                                                           }
+                                                           onClose={() => setShowDeleteAccount(false)}
+                                                           onDelete={onDelete}
+
+                        />}
+
+                        {canEdit && <tr>
+                            <td colSpan={2} style={{textAlign: "center"}}>
+                                <div style={{display: 'flex', alignItems: 'stretch', gap: 20}}>
+                                    {props.back !== undefined && <Button type={"button"} variant={"outlined"}
+                                                                         onClick={() => props.back && props.back()}>Back</Button>}
+                                    <div style={{display: 'flex', marginLeft: 'auto', gap: 20}}>
+
+                                        {props.account.id &&
+                                            <Button type={"button"} color={"primary"} variant={"outlined"}
+                                                    style={{marginLeft: 'auto'}} onClick={() => {
+                                                setShowResetPassword(true)
+                                            }}>Reset password</Button>}
+                                        {props.account.id &&
+                                            <Button type={"button"} color={"error"} variant={"outlined"}
+                                                    onClick={() => {
+                                                        setShowDeleteAccount(true)
+                                                    }}>Delete account</Button>}
+                                        <Button type={"submit"} color={"success"}
+                                                variant="outlined">Save</Button>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>}
                         </tbody>
                     </table>
                 </Form>)}
